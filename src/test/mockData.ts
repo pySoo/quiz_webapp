68b511e8f62851dcc1627d65803e3639d0affe6d
pySoo <soopy368@gmail.com>
2024-03-12 22:43:17 +0900

    [✨feat]: 테스트 코드에서 사용되는 mockData 추가

---
+++ src/test/mockData.ts
@@ -0,0 +1,136 @@
+export const MOCK_QUIZ_RESULT = [
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'Which sign of the zodiac is represented by the Crab?',
+    correct_answer: 'Cancer',
+    incorrect_answers: ['Libra', 'Virgo', 'Sagittarius'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'Which one of the following rhythm games was made by Harmonix?',
+    correct_answer: 'Rock Band',
+    incorrect_answers: [
+      'Meat Beat Mania',
+      'Guitar Hero Live',
+      'Dance Dance Revolution',
+    ],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'What do the letters of the fast food chain KFC stand for?',
+    correct_answer: 'Kentucky Fried Chicken',
+    incorrect_answers: [
+      'Kentucky Fresh Cheese',
+      'Kibbled Freaky Cow',
+      'Kiwi Food Cut',
+    ],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'Which of the following is not the host of a program on NPR?',
+    correct_answer: 'Ben Shapiro',
+    incorrect_answers: ['Terry Gross', 'Ira Glass', 'Peter Sagal'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question:
+      'What word represents the letter &#039;T&#039; in the NATO phonetic alphabet?',
+    correct_answer: 'Tango',
+    incorrect_answers: ['Target', 'Taxi', 'Turkey'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question:
+      'In the video-game franchise Kingdom Hearts, the main protagonist, carries a weapon with what shape?',
+    correct_answer: 'Key',
+    incorrect_answers: ['Sword', 'Pen', 'Cellphone'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'What is the Zodiac symbol for Gemini?',
+    correct_answer: 'Twins',
+    incorrect_answers: ['Fish', 'Scales', 'Maiden'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question:
+      'What type of animal was Harambe, who was shot after a child fell into it&#039;s enclosure at the Cincinnati Zoo?',
+    correct_answer: 'Gorilla',
+    incorrect_answers: ['Tiger', 'Panda', 'Crocodile'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'In past times, what would a gentleman keep in his fob pocket?',
+    correct_answer: 'Watch',
+    incorrect_answers: ['Money', 'Keys', 'Notebook'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'General Knowledge',
+    question: 'Red Vines is a brand of what type of candy?',
+    correct_answer: 'Licorice',
+    incorrect_answers: ['Lollipop', 'Chocolate', 'Bubblegum'],
+  },
+];
+
+export const MOCK_SELECTED_ANSWER = [
+  {
+    id: 0,
+    answer: 'Cancer',
+  },
+  {
+    id: 1,
+    answer: 'Meat Beat Mania',
+  },
+  {
+    id: 2,
+    answer: 'Kentucky Fried Chicken',
+  },
+  {
+    id: 3,
+    answer: 'Ira Glass',
+  },
+  {
+    id: 4,
+    answer: 'Target',
+  },
+  {
+    id: 5,
+    answer: 'Key',
+  },
+  {
+    id: 6,
+    answer: 'Scales',
+  },
+  {
+    id: 7,
+    answer: 'Panda',
+  },
+  {
+    id: 8,
+    answer: 'Keys',
+  },
+  {
+    id: 9,
+    answer: 'Licorice',
+  },
+];
